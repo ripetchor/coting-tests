d9dc6e16ce99df855c897918fcdf7eed15030335
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-01-25 14:21:41 +0500

    feat: update tests for some

--- test/some.spec.js
+++ test/some.spec.js
@@ -50,4 +50,77 @@ describe('some', () => {
 
     expect(result).toBe(expected);
   });
+
+  test.skip('should return true for value greater than 5', () => {
+    const expected = true;
+
+    function callback(v) {
+      return v > 5;
+    }
+
+    const result = some.call(ARRAY, callback);
+
+    expect(result).toBe(expected);
+  });
+
+  test.skip('should return true for value less than 2', () => {
+    const expected = true;
+
+    function callback(v) {
+      return v < 2;
+    }
+
+    const result = some.call(ARRAY, callback);
+
+    expect(result).toBe(expected);
+  });
+
+  test.skip('should return true for values divisible by 2', () => {
+    const expected = true;
+
+    function callback(v) {
+      return v % 2 === 0;
+    }
+
+    const result = some.call(ARRAY, callback);
+
+    expect(result).toBe(expected);
+  });
+
+  test.skip('should return false for all values greater than 10', () => {
+    const expected = false;
+
+    function callback(v) {
+      return v > 10;
+    }
+
+    const result = some.call(ARRAY, callback);
+
+    expect(result).toBe(expected);
+  });
+
+  test.skip('should return true for first value equal to 1', () => {
+    const expected = true;
+
+    function callback(v) {
+      return v === 1;
+    }
+
+    const result = some.call(ARRAY, callback);
+
+    expect(result).toBe(expected);
+  });
+
+  test.skip('should return false when all values are negative', () => {
+    const array = [-1, -2, -3];
+    const expected = false;
+
+    function callback(v) {
+      return v > 0;
+    }
+
+    const result = some.call(array, callback);
+
+    expect(result).toBe(expected);
+  });
 });

9c435518c3626c6051a26e7aae72d7174cc7d84c
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-01-29 18:20:45 +0500

    feat: add includes

---
+++ src/includes.js
@@ -0,0 +1,11 @@
+/**
+ * @template T
+ * @param {T} searchElement
+ * @param {number} [fromIndex]
+ * @returns {boolean}
+ */
+function includes(searchElement, fromIndex) {
+  // solution
+}
+
+module.exports = { includes };
---
+++ test/includes.spec.js
@@ -0,0 +1,73 @@
+const { includes } = require('../src/includes');
+
+describe('includes method tests', () => {
+  const ARRAY = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'];
+
+  test('should return true if the element is present in the array', () => {
+    const result = includes.call(ARRAY, 'c');
+
+    expect(result).toBe(true);
+  });
+
+  test('should return false if the element is not present in the array', () => {
+    const result = includes.call(ARRAY, 'z');
+
+    expect(result).toBe(false);
+  });
+
+  test('should return true if the element is present in the array starting from a specific index', () => {
+    const result = includes.call(ARRAY, 'c', 2);
+
+    expect(result).toBe(true);
+  });
+
+  test('should return true if the element is present in the array starting from a specific floating point number index', () => {
+    const result = includes.call(ARRAY, 'c', 2.9);
+
+    expect(result).toBe(true);
+  });
+
+  test('should return false if the element is not present in the array starting from a specific index', () => {
+    const result = includes.call(ARRAY, 'a', 3);
+
+    expect(result).toBe(false);
+  });
+
+  test('should handle negative fromIndex correctly', () => {
+    const result = includes.call(ARRAY, 'h', -3);
+
+    expect(result).toBe(true);
+  });
+
+  test('should return false if fromIndex is greater than or equal to the array length', () => {
+    const result = includes.call(ARRAY, 'a', 111);
+
+    expect(result).toBe(false);
+  });
+
+  test('should return true if the element is NaN and NaN is present in the array', () => {
+    const arrayWithNaN = [1, 2, NaN, 4];
+
+    const result = includes.call(arrayWithNaN, NaN);
+
+    expect(result).toBe(true);
+  });
+
+  test('should return false if the element is NaN and NaN is not present in the array', () => {
+    const arrayWithoutNaN = [1, 2, 3, 4];
+
+    const result = includes.call(arrayWithoutNaN, NaN);
+
+    expect(result).toBe(false);
+  });
+
+  test('should return false if the array is empty', () => {
+    const result = includes.call([], 'a');
+
+    expect(result).toBe(false);
+  });
+
+  test('should throw TypeError when called on a null object', () => {
+    expect(() => includes.call(null)).toThrow(TypeError);
+  });
+});
